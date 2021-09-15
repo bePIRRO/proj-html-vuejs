8b1b296dfff0df411d80b8b0edf49361c1bdaad7
bePIRRO <manuel.pirro@gmail.com>
2021-09-15 18:28:27 +0200

    add card's data

--- src/components/card.vue
+++ src/components/card.vue
@@ -3,6 +3,42 @@
 <script>
 export default {
   name: "Card",
+  data() {
+    return {
+      cards: [
+        {
+          url: "h5-custom-icon-1.png",
+          text: "Languages",
+          link: "#",
+        },
+        {
+          url: "h5-custom-icon-2.png",
+          text: "Software",
+          link: "#",
+        },
+        {
+          url: "h5-custom-icon-3.png",
+          text: "Business",
+          link: "#",
+        },
+        {
+          url: "h5-custom-icon-4.png",
+          text: "Chemistry",
+          link: "#",
+        },
+        {
+          url: "h5-custom-icon-5.png",
+          text: "Science",
+          link: "#",
+        },
+        {
+          url: "h5-custom-icon-6.png",
+          text: "DIY%Craft",
+          link: "#",
+        },
+      ],
+    };
+  },
   props: {},
 };
 </script>
